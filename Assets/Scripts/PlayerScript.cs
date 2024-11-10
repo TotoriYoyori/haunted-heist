fb76f14f984c03f71171c63cced0812f6365f81b
Igors <igors.sorogovecs.8360@student.uu.se>
2024-11-10 22:28:54 +0100

    Connecting two players works

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,12 +8,25 @@ public class PlayerScript : NetworkBehaviour
     [SerializeField] float speed;
     [SerializeField] bool is_robber;
     [SerializeField] GameObject player_camera;
+    [SerializeField] GameObject this_robber;
+    [SerializeField] GameObject this_ghost;
+ 
     public bool frozen;
     // Start is called before the first frame update
     void Awake()
     {
-        if (is_robber) Game.robber = this.gameObject;
-        else Game.ghost = this.gameObject;
+        //if (is_robber) Game.robber = this.gameObject;
+        if (Game.robber == null)
+        {
+            Game.robber = this.gameObject;
+            is_robber = true;
+            this_robber.SetActive(true);
+        }
+        else
+        {
+            Game.ghost = this.gameObject;
+            this_ghost.SetActive(true);
+        }
         //Instantiate(player_camera);
         Game.player = this.gameObject;
     }
@@ -37,36 +50,36 @@ public class PlayerScript : NetworkBehaviour
         // Left Mouse Abilities (NightVision and ChargeAttack)
         if (Input.GetButtonDown("Fire1"))
         {
-            if (is_robber) GetComponent<RobberScript>().NightVision(true);
-            else GetComponent<GhostScript>().ChargeAttack(true);
+            if (is_robber) this_robber.GetComponent<RobberScript>().NightVision(true);
+            else this_ghost.GetComponent<GhostScript>().ChargeAttack(true);
         }
         if (Input.GetButtonUp("Fire1"))
         {
-            if (is_robber) GetComponent<RobberScript>().NightVision(false);
-            else GetComponent<GhostScript>().ChargeAttack(false);
+            if (is_robber) this_robber.GetComponent<RobberScript>().NightVision(false);
+            else this_ghost.GetComponent<GhostScript>().ChargeAttack(false);
         }
 
         // Right Mouse Abilities (Flashlight and StepVision)
         if (Input.GetButtonDown("Fire2"))
         {
-            if (is_robber) GetComponent<RobberScript>().Flashlight(true);
-            else GetComponent<GhostScript>().StepVision(true);
+            if (is_robber) this_robber.GetComponent<RobberScript>().Flashlight(true);
+            else this_ghost.GetComponent<GhostScript>().StepVision(true);
         }
         if (Input.GetButtonUp("Fire2"))
         {
-            if (is_robber) GetComponent<RobberScript>().Flashlight(false);
-            else GetComponent<GhostScript>().StepVision(false);
+            if (is_robber) this_robber.GetComponent<RobberScript>().Flashlight(false);
+            else this_ghost.GetComponent<GhostScript>().StepVision(false);
         }
 
         // C button abilities (Item pickUp and walking through walls)
         if (Input.GetButtonDown("Jump"))
         {
-            if (is_robber) GetComponent<RobberScript>().ItemPickUpAura.GetComponent<ItemPickUp>().StartPicking(true);
+            if (is_robber) this_robber.GetComponent<RobberScript>().ItemPickUpAura.GetComponent<ItemPickUp>().StartPicking(true);
             //else GetComponent<GhostScript>().StepVision(false);
         }
         if (Input.GetButtonUp("Jump"))
         {
-           if (is_robber) GetComponent<RobberScript>().ItemPickUpAura.GetComponent<ItemPickUp>().StartPicking(false);
+           if (is_robber) this_robber.GetComponent<RobberScript>().ItemPickUpAura.GetComponent<ItemPickUp>().StartPicking(false);
             //else GetComponent<GhostScript>().StepVision(false);
         }
     }
